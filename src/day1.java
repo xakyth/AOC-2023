bfab350a86bc8522ffbfe607d395843e6eb19365
xakyth <xakyth@gmail.com>
2023-12-05 19:05:33 +0300

    day1 - 2 part

--- src/day1.java
+++ src/day1.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class day1 {
   public static void main(String[] args) throws IOException {
-    List<String> firstInput = Files.readAllLines(Paths.get("src", "day1_input1.txt"));
-    System.out.println(sumOfCalibrationValues(firstInput));
+    List<String> input = Files.readAllLines(Paths.get("src", "day1_input1.txt"));
+    System.out.println("part1: " + sumOfCalibrationValues(input));
+    System.out.println("part2: " + sumOfCalibrationValuesDigitsAsWords(input));
   }
 
   public static int sumOfCalibrationValues(List<String> allLines) {
@@ -28,6 +29,62 @@ public class day1 {
       }
       totalSum += firstDigit * 10 + lastDigit;
     }
+    return totalSum;
+  }
+
+  public static int sumOfCalibrationValuesDigitsAsWords(List<String> allLines) {
+    int totalSum = 0;
+    String[] digits = new String[] {
+        "one", "two", "three",
+        "four", "five", "six",
+        "seven", "eight", "nine" };
+
+    for (String line : allLines) {
+      int[] firstOccurences = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+      int[] lastOccurences = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+      for (int i = 0; i < 9; i++) {
+        firstOccurences[i] = line.indexOf(digits[i]);
+        int occ = line.indexOf(Integer.toString(i+1));
+        if (firstOccurences[i] == -1) {
+          firstOccurences[i] = occ;
+        } 
+        else {
+          if (occ != -1 && occ < firstOccurences[i])
+            firstOccurences[i] = occ;
+        }
+      }
+      for (int i = 0; i < 9; i++) {
+        lastOccurences[i] = line.lastIndexOf(digits[i]);
+        int occ = line.lastIndexOf(Integer.toString(i+1));
+        if (lastOccurences[i] == -1) {
+          lastOccurences[i] = occ;
+        } 
+        else {
+          if (occ != -1 && occ > lastOccurences[i])
+            lastOccurences[i] = occ;
+        }
+      }
+
+      int temp = line.length();
+      int temp2 = 0;
+      for (int i = 0; i < firstOccurences.length; i++) {
+        if (firstOccurences[i] != -1 && firstOccurences[i] < temp) {
+          temp = firstOccurences[i];
+          temp2 = i + 1;
+        }
+      }
+      int firstDigit = temp2;
+      temp = -1;
+      for (int i = 0; i < lastOccurences.length; i++) {
+        if (lastOccurences[i] > temp) {
+          temp = lastOccurences[i];
+          temp2 = i + 1;
+        }
+      }
+      int lastDigit = temp2;
+      totalSum += firstDigit * 10 + lastDigit;
+    }
+
     return totalSum;
   }
 }
