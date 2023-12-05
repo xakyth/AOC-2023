a3687caf64afbb2da6d048d96c7af601d83ce290
xakyth <xakyth@gmail.com>
2023-12-05 19:19:33 +0300

    day2 - part1

---
+++ src/day2.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class day2 {
+  public static void main(String[] args) throws IOException {
+    List<String> input = Files.readAllLines(Paths.get("src", "day2_input.txt"));
+    System.out.println("part1: " + part1(input));
+  }
+
+  public static int part1(List<String> lines) {
+    int sumOfIds = 0;
+    int red = 12, green = 13, blue = 14;
+    Pattern pattern = Pattern.compile("(\\d+) (red|green|blue)");
+    for (String line : lines) {
+      int gameId = Integer.parseInt(line.substring(5, line.indexOf(':')));
+      line = line.substring(line.indexOf(':') + 1);
+      String[] sets = line.split(";");
+      boolean flag = true;
+      for (String set : sets) {
+        Matcher matcher = pattern.matcher(set);
+        while (matcher.find()) {
+          int numOfCubes = Integer.parseInt(matcher.group(1));
+          switch (matcher.group(2)) {
+            case "red":
+              if (numOfCubes > red)
+                flag = false;
+              break;
+            case "green":
+              if (numOfCubes > green)
+                flag = false;
+              break;
+            case "blue":
+              if (numOfCubes > blue)
+                flag = false;
+              break;
+          }
+        }
+        if (!flag)
+          break;
+      }
+      if (flag) {
+        sumOfIds += gameId;
+      }
+    }
+
+    return sumOfIds;
+  }
+}
