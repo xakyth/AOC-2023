644c6a55285fc09e93e1fb3a153f7067580b8996
xakyth <xakyth@gmail.com>
2023-12-06 19:13:18 +0300

    day2.p2

--- src/day2.java
+++ src/day2.java
@@ -9,6 +9,7 @@ public class day2 {
   public static void main(String[] args) throws IOException {
     List<String> input = Files.readAllLines(Paths.get("src", "day2_input.txt"));
     System.out.println("part1: " + part1(input));
+    System.out.println("part2: " + part2(input));
   }
 
   public static int part1(List<String> lines) {
@@ -48,5 +49,37 @@ public class day2 {
     }
 
     return sumOfIds;
+  }
+  public static int part2(List<String> lines) {
+    int totalSum = 0;
+    Pattern pattern = Pattern.compile("(\\d+) (red|green|blue)");
+    for (String line : lines) {
+      int red = 0, green = 0, blue = 0;
+      line = line.substring(line.indexOf(':') + 1);
+      String[] sets = line.split(";");
+      for (String set : sets) {
+        Matcher matcher = pattern.matcher(set);
+        while (matcher.find()) {
+          int numOfCubes = Integer.parseInt(matcher.group(1));
+          switch (matcher.group(2)) {
+            case "red":
+              if (numOfCubes > red)
+                red = numOfCubes;
+              break;
+            case "green":
+              if (numOfCubes > green)
+                green = numOfCubes;
+              break;
+            case "blue":
+              if (numOfCubes > blue)
+                blue = numOfCubes;
+              break;
+          }
+        }
+      }
+      totalSum += red * green * blue;
+    }
+
+    return totalSum;
   }
 }
